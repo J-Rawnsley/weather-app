2b6d34fb2cd36e69e2e5284bd5d9984bd849bd04
Joe Rawnsley <joe.rawnsley@gmail.com>
2023-09-10 18:23:24 +0100

    updated 'type' in package.json to 'commonjs'

--- netlify/functions/token-hider/token-hider.js
+++ netlify/functions/token-hider/token-hider.js
@@ -5,6 +5,8 @@ const qs = require('qs')
 
 const handler = async function (event) {
   // apply our function to the queryStringParameters and assign it to a variable
+  console.log("function accessed")
+  
   const API_PARAMS = qs.stringify(event.queryStringParameters)
   console.log('API_PARAMS', API_PARAMS)
   // Get env var values defined in our Netlify site UI
