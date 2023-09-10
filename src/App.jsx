0c9a199cd10f722753d63c045979b6609f421dba
Joe Rawnsley <joe.rawnsley@gmail.com>
2023-09-10 16:55:55 +0100

    changed to temporary key for testing purposes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     lon: "02.24",
   });
 
-  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${selected.lat}&lon=${selected.lon}&units=metric&appid=fdcf7d03afbc001fee4ba948237bc9c3`;
+  const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${selected.lat}&lon=${selected.lon}&units=metric&appid=e1e58e775c7a8102c7403222ae0ca285`;
 
   const [weatherData, setWeatherData] = useState([]);
 
