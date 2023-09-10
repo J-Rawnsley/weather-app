4b81b9dc61b6bd5a37f2853ed03f914f8ec88a61
Joe Rawnsley <joe.rawnsley@gmail.com>
2023-09-10 21:19:36 +0100

    fixed bugs ready for deployment with new hidden API key

--- netlify/functions/token-hider/token-hider.js
+++ netlify/functions/token-hider/token-hider.js
@@ -1,28 +1,21 @@
-const process = require('process')
+const process = require("process");
 
-const axios = require('axios')
-const qs = require('qs')
+const axios = require("axios");
+const qs = require("qs");
 
 const handler = async function (event) {
-  // apply our function to the queryStringParameters and assign it to a variable
-  console.log("function accessed")
-  
-  const API_PARAMS = qs.stringify(event.queryStringParameters)
-  console.log('API_PARAMS', API_PARAMS)
-  // Get env var values defined in our Netlify site UI
+  console.log("function accessed");
 
-  // TODO: customize your URL and API keys set in the Netlify Dashboard
-  // this is secret too, your frontend won't see this
-  const { API_KEY } = process.env
-  const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${API_PARAMS.lat}&lon=${API_PARAMS.lon}&units=metric&appid=${API_KEY}`
+  const API_PARAMS = event.queryStringParameters;
+  console.log("API_PARAMS", API_PARAMS);
 
-  console.log('Constructed URL is ...', URL)
+  const { API_KEY } = process.env;
+  const URL = `https://api.openweathermap.org/data/2.5/forecast?lat=${API_PARAMS.lat}&lon=${API_PARAMS.lon}&units=metric&appid=${API_KEY}`;
+  console.log("Constructed URL is ...", URL);
 
   try {
-    const { data } = await axios.get(URL)
-    // refer to axios docs for other methods if you need them
-    // for example if you want to POST data:
-    //    axios.post('/user', { firstName: 'Fred' })
+    const { data } = await axios.get(URL);
+
     return {
       statusCode: 200,
       body: JSON.stringify(data),
@@ -30,15 +23,15 @@ const handler = async function (event) {
         "Access-Control-Allow-Origin": "*",
         "Access-Control-Allow-Headers": "Content-Type",
         "Access-Control-Allow-Methods": "GET, POST, OPTION",
-      }
-    }
+      },
+    };
   } catch (error) {
-    const { data, headers, status, statusText } = error.response
+    const { data, headers, status, statusText } = error.response;
     return {
       statusCode: error.response.status,
       body: JSON.stringify({ status, statusText, headers, data }),
-    }
+    };
   }
-}
+};
 
-module.exports = { handler }
+module.exports = { handler };
--- src/App.jsx
+++ src/App.jsx
@@ -12,9 +12,12 @@ function App() {
     lon: "02.24",
   });
 
-  // const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${selected.lat}&lon=${selected.lon}&units=metric&appid=e1e58e775c7a8102c7403222ae0ca285`;
 
-  const url = `https://jrawnsley-weather.netlify.app/.netlify/functions/token-hider?lat=${selected.lat}&lon=${selected.lon}`
+  // const url = `https://jrawnsley-weather.netlify.app/.netlify/functions/token-hider?lat=${selected.lat}&lon=${selected.lon}`
+
+  const url = `http://localhost:8888/.netlify/functions/token-hider?lat=${selected.lat}&lon=${selected.lon}`
+
+
   const [weatherData, setWeatherData] = useState([]);
 
   const getData = () => {
@@ -38,7 +41,6 @@ function App() {
   return (
     <div id="appContainer">
       <h1>How's The Weather?</h1>
-      <h2>In Development</h2>
       <div id="buttons-container">
         <ButtonsArray data={locations} setFunction={setSelected} />
       </div>
